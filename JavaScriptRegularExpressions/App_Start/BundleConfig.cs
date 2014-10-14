5ce78dca91646896d7a9853643a0467b6d5ff261
decherneyge <George.DeCherney@gmail.com>
2014-10-13 21:28:52 -0400

    replaced with working version

--- JavaScriptRegularExpressions/App_Start/BundleConfig.cs
+++ JavaScriptRegularExpressions/App_Start/BundleConfig.cs
@@ -1,30 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BundleTransformer.Core.Bundles;
+using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Transformers;
 using System.Web;
 using System.Web.Optimization;
 
-namespace JavaScriptRegularExpressions.App_Start
+namespace JavaScriptRegularExpressions
 {
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = false;
+
+            bundles.UseCdn = true;
+            var cssTransformer = new CssTransformer();
+            var jsTransformer = new JsTransformer();
+            var nullOrderer = new NullOrderer();
+
+            var cssBundle = new CustomStyleBundle("~/bundles/css");
+            cssBundle.Include("~/Content/Site.less", "~/Content/bootstrap/bootstrap.less");
+            cssBundle.Transforms.Add(cssTransformer);
+            cssBundle.Orderer = nullOrderer;
+            bundles.Add(cssBundle);
+
+            var jqueryBundle = new CustomScriptBundle("~/bundles/jquery");
+            jqueryBundle.Include("~/Scripts/jquery-{version}.js");
+            jqueryBundle.Transforms.Add(jsTransformer);
+            jqueryBundle.Orderer = nullOrderer;
+            bundles.Add(jqueryBundle);
+
+            var jqueryvalBundle = new CustomScriptBundle("~/bundles/jqueryval");
+            jqueryvalBundle.Include("~/Scripts/jquery.validate*");
+            jqueryvalBundle.Transforms.Add(jsTransformer);
+            jqueryvalBundle.Orderer = nullOrderer;
+            bundles.Add(jqueryvalBundle);
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            var modernizrBundle = new CustomScriptBundle("~/bundles/modernizr");
+            modernizrBundle.Include("~/Scripts/modernizr-*");
+            modernizrBundle.Transforms.Add(jsTransformer);
+            modernizrBundle.Orderer = nullOrderer;
+            bundles.Add(modernizrBundle);
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            var bootstrapBundle = new CustomScriptBundle("~/bundles/bootstrap");
+            bootstrapBundle.Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js");
+            bootstrapBundle.Transforms.Add(jsTransformer);
+            bootstrapBundle.Orderer = nullOrderer;
+            bundles.Add(bootstrapBundle);
+
+
         }
     }
 }
--- JavaScriptRegularExpressions/Global.asax.cs
+++ JavaScriptRegularExpressions/Global.asax.cs
@@ -1,8 +1,8 @@
-﻿using JavaScriptRegularExpressions.App_Start;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,10 +14,10 @@ namespace JavaScriptRegularExpressions
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
         }
     }
 }
